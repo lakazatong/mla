23e47b13ac4b0742cb2cb652e1b2b8063227863f
lakazatong <Bo_wood@outlook.fr>
2024-05-29 13:30:15 +0200

    some progress

--- profile_cpp2.py
+++ profile_cpp2.py
@@ -21,10 +21,83 @@ class File:
 	def __init__(self, lines, functions):
 		self.lines = lines
 		self.functions = functions
+		self.filename = "mla"
 
 file = File([SingleLine(2, " al lo "), FunctionLine(3, "aluile")], [Function(1, 2, 3), Function(1, 2, 3)])
 
 a = f"""
-	functions_t functions{{ {", ".join(f"function_t({function.first_line_number}, {function.last_line_number}, {function.longest_line_number})" for function in file.functions)} }};
+	""yo""
 """
-print(a)
+# print(a)
+
+content = ""
+
+# ----------------------------------------------------------------------
+# parameters
+
+profile_namespace = "Profiled"
+
+# parameters
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# profile.hpp
+
+profile_hpp = f"""
+
+"""
+
+# profile.hpp
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# profile.cpp
+
+profile_hpp = f"""
+
+"""
+
+# profile.cpp
+# ----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
+# profile_{filename}.cpp
+
+with open("profile_{filename}.cpp", "r") as f:
+	content = f.read()
+
+profile_filename_cpp = lambda file: content.format(profile_namespace=profile_namespace, file_filename=file.filename)
+
+# profile_{filename}.cpp
+# ----------------------------------------------------------------------
+
+print(profile_filename_cpp(file))
+
+exit(0)
+
+# string to put before functions / lines to add profiling to
+profile_function_pattern = re.compile(r"\/\/ @profile\s+function\s*\r?\n")
+profile_line_pattern = re.compile(r"\/\/ @profile\s+line\s*\r?\n")
+# lines to ignore
+ignore_patterns = [
+	re.compile(r"^\s*[{}]+\s*$"), # ignore only brackets
+	re.compile(r"^\s*//(?:a|[^a])*$"), # ignore single line comments
+	re.compile(r"^\s*return;\s*$") # ignore empty returns
+]
+# line reports
+report_line_pattern = re.compile(rf"\/\/ {report_line_macro_name}\((\d+)\)")
+# function reports (any character is captured, the function name check will be performed after)
+report_function_pattern = re.compile(rf"\/\/ {report_function_macro_name}\(((?:a|[^a])+?)\)")
+# chrono include
+# a = re.compile(r"#include <chrono>")
+
+def main():
+	if len(sys.argv) < 2:
+		print("provide cpp files")
+		return
+
+	for file_path in sys.argv[1:]:
+		handle_file_path(file_path, file_path.split('.')[0] + '_profiled.cpp')
+
+if __name__ == "__main__":
+	main()
